1d9805c409f324f3d563744705820b6b5f539fb2
Nasr <larko236@outlook.com>
2025-05-15 16:56:54 +0800

    parallelize erc1155 by contract

--- crates/processors/src/processors/erc1155_transfer_single.rs
+++ crates/processors/src/processors/erc1155_transfer_single.rs
@@ -1,3 +1,5 @@
+use std::hash::{DefaultHasher, Hash, Hasher};
+
 use anyhow::Error;
 use async_trait::async_trait;
 use cainome::cairo_serde::{CairoSerde, U256 as U256Cainome};
@@ -7,7 +9,7 @@ use starknet::providers::Provider;
 use torii_sqlite::Sql;
 use tracing::debug;
 
-use crate::task_manager::{self, TaskId, TaskPriority};
+use crate::task_manager::{TaskId, TaskPriority};
 use crate::{EventProcessor, EventProcessorConfig};
 
 pub(crate) const LOG_TARGET: &str = "torii::indexer::processors::erc1155_transfer_single";
@@ -34,8 +36,10 @@ where
         1
     }
 
-    fn task_identifier(&self, _event: &Event) -> TaskId {
-        task_manager::TASK_ID_SEQUENTIAL
+    fn task_identifier(&self, event: &Event) -> TaskId {
+        let mut hasher = DefaultHasher::new();
+        event.from_address.hash(&mut hasher);
+        hasher.finish()
     }
 
     async fn process(
