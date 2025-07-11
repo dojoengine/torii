e5000d75a53b3875c113ed0e98c1ad038b07d810
Larko <59736843+Larkooo@users.noreply.github.com>
2025-07-11 18:27:32 +0800

    refactor(proxy): http2 alpn protocol for gql and grpc self cert (#178)
    * refactor(proxy): http2 only
    
    * h2 alpn protocol
    
    * h2 gql

--- crates/server/src/proxy.rs
+++ crates/server/src/proxy.rs
@@ -59,6 +59,7 @@ lazy_static::lazy_static! {
     pub(crate) static ref GRAPHQL_PROXY_CLIENT: ReverseProxy<HttpConnector<GaiResolver>> = {
         ReverseProxy::new(
             Client::builder()
+             .http2_only(true)
              .build_http(),
         )
     };
@@ -148,10 +149,12 @@ impl Proxy {
         let key = PrivateKey(keys[0].clone());
 
         // Create server config
-        let server_config = ServerConfig::builder()
+        let mut server_config = ServerConfig::builder()
             .with_safe_defaults()
             .with_no_client_auth()
             .with_single_cert(certs, key)?;
+        server_config.alpn_protocols =
+            vec![b"h2".to_vec(), b"http/1.1".to_vec(), b"http/1.0".to_vec()];
 
         Ok(server_config)
     }
