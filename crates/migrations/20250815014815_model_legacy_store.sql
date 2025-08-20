bbc9cb6b608383d544308072ce2a67d444cb63a6
Nasr <larko236@outlook.com>
2025-08-20 10:53:54 +0100

    fix migratiojn

--- crates/migrations/20250815014815_model_legacy_store.sql
+++ crates/migrations/20250815014815_model_legacy_store.sql
@@ -1,4 +1,4 @@
 -- Models now have a different serialized format, we add a new column `legacy_store` to
 -- distinguish models that use the legacy format. This is important for backward compatibility.
 ALTER TABLE models
-ADD COLUMN legacy_store BOOLEAN NOT NULL;
+ADD COLUMN legacy_store BOOLEAN DEFAULT FALSE;
