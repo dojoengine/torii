faf5f9cd94cff49e81ca138cd608860af538b5c8
Nasr <larko236@outlook.com>
2025-09-01 13:29:05 +0100

    f

--- crates/indexer/engine/src/test.rs
+++ crates/indexer/engine/src/test.rs
@@ -64,7 +64,7 @@ where
     let data = fetcher.fetch(&cursors).await.unwrap();
     engine.process(&data).await.unwrap();
 
-    db.apply_balances_diff(cache.balances_diff().await, cursors)
+    db.apply_balances_diff(cache.balances_diff().await, cache.total_supply_diff().await, cursors)
         .await
         .unwrap();
     db.execute().await.unwrap();
