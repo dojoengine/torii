04a7733b90f47a10547520dbc3990461e8b510ad
Larko <59736843+Larkooo@users.noreply.github.com>
2025-10-08 12:45:07 -0500

    refactor(processors): try retrieve model from storage that uses cache (#353)

--- crates/processors/src/processors/upgrade_event.rs
+++ crates/processors/src/processors/upgrade_event.rs
@@ -60,7 +60,7 @@ where
 
         // If the model does not exist, silently ignore it.
         // This can happen if only specific namespaces are indexed.
-        let model = match ctx.cache.model(event.selector).await {
+        let model = match ctx.storage.model(event.selector).await {
             Ok(m) => m,
             Err(e) if e.to_string().contains("no rows") => {
                 debug!(
